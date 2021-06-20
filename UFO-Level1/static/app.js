fc9c2ee422745236563a035869b5055bcd19dad6
Amanda Troxel <amandatroxel2@gmail.com>
2021-06-19 23:25:17 -0600

    table columns

--- UFO-Level1/static/app.js
+++ UFO-Level1/static/app.js
@@ -2,4 +2,18 @@ var tableData = data;
 
 for (var i = 0; i < tableData.length; i++) {
     console.log(tableData[i]);
-}
+};
+
+var Columns = ["Date", "City", "State", "Country", "Shape", "Duration", "Comments"];
+
+var tbody = d3.select("tbody");
+tableData.forEach(function(newUFO) {
+    console.log(newUFO);
+    var tableRow = tbody.append("tr");
+    Columns.forEach(Column => tableRow.append("td").text(newUFO[Column])
+    )
+});
+
+
+
+
